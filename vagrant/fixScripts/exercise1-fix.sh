2db80e4c3523db5596a9bae60123f5aded7a29f1
ynonye <ynonye@gmail.com>
2019-01-21 21:42:49 +0200

    answers

--- vagrant/fixScripts/exercise1-fix.sh
+++ vagrant/fixScripts/exercise1-fix.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 #add fix to exercise1 here
+sudo sh -c "grep -q 'nameserver 8.8.8.8' /etc/resolv.conf || echo 'nameserver 8.8.8.8' >> /etc/resolv.conf"
+sudo /etc/init.d/networking restart
--- vagrant/fixScripts/exercise4-fix_server1.sh
+++ vagrant/fixScripts/exercise4-fix_server1.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 #add fix to exercise4-server1 here
+sudo sh -c "echo 192.168.100.11 server2 >> etc/hosts"
